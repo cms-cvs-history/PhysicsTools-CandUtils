b1a31989a4f375448e335939e0c58ee4d4dff7c3
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2008-05-06 07:59:24 +0000

    simplified cand combiner using View<Candidate> as input collection

--- test/testCandCombiner.cc
+++ test/testCandCombiner.cc
@@ -1,6 +1,7 @@
-// $Id: testCandCombiner.cc,v 1.3 2007/07/23 10:04:14 llista Exp $
+// $Id: testCandCombiner.cc,v 1.4 2008/03/18 12:51:55 wmtan Exp $
 #include <cppunit/extensions/HelperMacros.h>
 #include "DataFormats/Candidate/interface/LeafCandidate.h"
+#include "DataFormats/Candidate/interface/CompositeCandidate.h"
 #include "PhysicsTools/CandUtils/interface/CandCombiner.h"
 #include "PhysicsTools/Utilities/interface/AnySelector.h"
 #include "DataFormats/Common/test/TestHandle.h"
@@ -22,23 +23,21 @@ CPPUNIT_TEST_SUITE_REGISTRATION(testCandCombiner);
 void testCandCombiner::checkAll() {
   using namespace std;
   using namespace edm;
-  CandidateCollection c;
-  edm::TestHandle<CandidateCollection> hd(&c, ProductID(1));
-  CandidateRefProd cands( hd );
-  auto_ptr<Candidate> c1( new LeafCandidate( +1, math::XYZTLorentzVector( 1, 0, 0, 1 ) ) );
-  auto_ptr<Candidate> c2( new LeafCandidate( -1, math::XYZTLorentzVector( 0, 1, 0, 1 ) ) );
-  c.push_back( c1 );
-  c.push_back( c2 );
-  CandCombiner<CandidateCollection, AnySelector> combiner1( +1, -1 );
-  auto_ptr<CandidateCollection> cc1 = combiner1.combine( cands );
-  CPPUNIT_ASSERT( cc1->size() == 1 );
-  CandCombiner<CandidateCollection, AnySelector> combiner2;
-  auto_ptr<CandidateCollection> cc2 = combiner2.combine( cands );
-  CPPUNIT_ASSERT( cc2->size() == 1 );
-  auto_ptr<Candidate> c3( new LeafCandidate( +1, math::XYZTLorentzVector( 0, 0, 1, 1 ) ) );
-  c.push_back( c3 );
-  auto_ptr<CandidateCollection> cc3 = combiner1.combine( cands );
-  CPPUNIT_ASSERT( cc3->size() == 2 );
-  auto_ptr<CandidateCollection> cc4 = combiner2.combine( cands );
-  CPPUNIT_ASSERT( cc4->size() == 3 );
+  typedef vector<LeafCandidate> LeafCandidateCollection;
+  LeafCandidateCollection c;
+  edm::TestHandle<LeafCandidateCollection> hd(&c, ProductID(1));
+  RefProd<LeafCandidateCollection> cands(hd);
+  c.push_back(LeafCandidate(+1, math::XYZTLorentzVector(1, 0, 0, 1)));
+  c.push_back(LeafCandidate(-1, math::XYZTLorentzVector(0, 1, 0, 1)));
+  CandCombiner<AnySelector> combiner1(+1, -1);
+  auto_ptr<CompositeCandidateCollection> cc1 = combiner1.combine(cands);
+  CPPUNIT_ASSERT(cc1->size() == 1);
+  CandCombiner<AnySelector> combiner2;
+  auto_ptr<CompositeCandidateCollection> cc2 = combiner2.combine(cands);
+  CPPUNIT_ASSERT(cc2->size() == 1);
+  c.push_back(LeafCandidate(+1, math::XYZTLorentzVector(0, 0, 1, 1)));
+  auto_ptr<CompositeCandidateCollection> cc3 = combiner1.combine(cands);
+  CPPUNIT_ASSERT(cc3->size() == 2);
+  auto_ptr<CompositeCandidateCollection> cc4 = combiner2.combine(cands);
+  CPPUNIT_ASSERT(cc4->size() == 3);
 }
